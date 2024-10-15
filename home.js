12dbab22be8d64996f432b9ff7856d3e7fd1e285
Treplla <treplla@yahoo.com>
2024-10-14 22:08:28 -0300

    Update home.js

--- home.js
+++ home.js
@@ -1,17 +1,17 @@
-// Tema
+// Alternar entre tema claro e escuro
 document.getElementById('theme-toggle').addEventListener('click', function() {
-  document.body.classList.toggle('dark-mode');
-  const icon = document.getElementById('theme-icon');
-  if (document.body.classList.contains('dark-mode')) {
-    icon.classList.remove('fa-sun');
-    icon.classList.add('fa-moon');
-  } else {
-    icon.classList.remove('fa-moon');
-    icon.classList.add('fa-sun');
-  }
+    document.body.classList.toggle('dark-mode');
+    const icon = document.getElementById('theme-icon');
+    if (document.body.classList.contains('dark-mode')) {
+        icon.classList.remove('fa-sun');
+        icon.classList.add('fa-moon');
+    } else {
+        icon.classList.remove('fa-moon');
+        icon.classList.add('fa-sun');
+    }
 });
 
-// sidebar
+// Mostrar ou esconder a sidebar
 document.getElementById('menu-icon').addEventListener('click', function() {
-  document.getElementById('sidebar').classList.toggle('active');
+    document.getElementById('sidebar').classList.toggle('active');
 });
