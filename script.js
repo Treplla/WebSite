42a23b7d71553047eaebbff6645e24112bbabed6
Treplla <treplla@yahoo.com>
2024-10-16 00:47:14 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    setTimeout(function() {
+        var loading = document.getElementById('loading');
+        loading.style.display = 'none';
+    }, 4000); // Ajuste este tempo conforme necessário para sincronizar com a animação
+};
