6443b8eafd95ce37b686d88b3dc50c6a6e91674d
Treplla <treplla@yahoo.com>
2024-10-16 21:43:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,8 @@ window.onload = function() {
     setTimeout(function() {
         var loading = document.getElementById('loading');
         loading.style.display = 'none';
+        
+        // Redireciona para a página home.html após ocultar o elemento de carregamento
+        window.location.href = 'home.html';
     }, 5000); // Ajuste o tempo conforme necessário para sincronizar com a animação
 };
