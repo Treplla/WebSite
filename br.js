a9052a9570faef87d97041dc726a5d24e29d8572
Treplla <treplla@yahoo.com>
2024-10-17 00:03:51 -0300

    Create br.js

---
+++ br.js
@@ -0,0 +1,17 @@
+// Alterna entre modo claro e escuro
+document.getElementById('theme-toggle').addEventListener('click', function() {
+  document.body.classList.toggle('dark-mode');
+  const icon = document.getElementById('theme-icon');
+  if (document.body.classList.contains('dark-mode')) {
+    icon.classList.remove('fa-sun');
+    icon.classList.add('fa-moon');
+  } else {
+    icon.classList.remove('fa-moon');
+    icon.classList.add('fa-sun');
+  }
+});
+
+// Alterna a sidebar
+document.getElementById('menu-icon').addEventListener('click', function() {
+  document.getElementById('sidebar').classList.toggle('active');
+});
